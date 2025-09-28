e62e8654dc6e0d8d0e936a92cd11e0d366f5cba4
Soyeon <isylsy166@gmail.com>
2025-09-29 01:06:36 +0900

    [chore] (#18): SpringBatch로 RSS 불러오기, 저장. read-process-writer

---
+++ src/main/java/com/uphouse/monew/domain/article/mapper/RssArticleMapper.java
@@ -0,0 +1,15 @@
+package com.uphouse.monew.domain.article.mapper;
+
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.uphouse.monew.domain.article.dto.RssArticle;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface RssArticleMapper {
+    @Mapping(source = "title", target = "title")
+    @Mapping(source = "link", target = "link")
+    @Mapping(source = "author", target = "author")
+    @Mapping(source = "publishedDate", target = "publishedDate")
+    RssArticle toDto(SyndEntry entry);
+}
