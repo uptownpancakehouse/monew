846b2979ca6b4ff3a9954aba69173d3f0a7c7889
Soyeon <isylsy166@gmail.com>
2025-09-29 16:24:44 +0900

    [feat] (#26): batch processor에 Set 추가해서 중복 저장 방지

--- src/main/java/com/uphouse/monew/infrastructure/batch/processor/YonhapRssProcessor.java
+++ src/main/java/com/uphouse/monew/infrastructure/batch/processor/YonhapRssProcessor.java
@@ -3,12 +3,18 @@ package com.uphouse.monew.infrastructure.batch.processor;
 import com.uphouse.monew.domain.article.domain.Article;
 import com.uphouse.monew.domain.article.dto.RssArticle;
 import com.uphouse.monew.domain.article.dto.SourceType;
+import com.uphouse.monew.domain.article.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Slf4j
 @Component
@@ -16,8 +22,23 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class YonhapRssProcessor implements ItemProcessor<RssArticle, Article>, StepExecutionListener {
 
+    private final ArticleRepository articleRepository;
+    private final Set<String> yonhapArticleSet = new HashSet<>();
+
+    @Override
+    public void beforeStep(StepExecution stepExecution) {
+        yonhapArticleSet.clear();
+        List<Article> articles = articleRepository.findBySource(SourceType.YONHAP);
+        articles.forEach(article -> yonhapArticleSet.add(article.getSourceUrl()));
+        log.info("중복 체크 Set 초기화 완료, 현재 크기={}", yonhapArticleSet.size());
+    }
+
     @Override
     public Article process(RssArticle article) throws Exception {
+
+        if(yonhapArticleSet.contains(article.link())) return null;
+
+        yonhapArticleSet.add(article.link());
 
         return Article.builder()
                 .sourceUrl(article.link())
